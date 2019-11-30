659dc744adcb2828293206ebd795243731de2a82
Zeeshan Hooda <zeeshanhooda@icloud.com>
2019-11-29 20:46:11 -0700

    Started basic kernel and loaded kernel in bootsect

--- src/boot_sect_main.asm
+++ src/boot_sect_main.asm
@@ -1,32 +1,50 @@
 [org 0x7c00] ; tell the assembler that our offset is bootsector code
-    mov bp, 0x8000 ; set the stack safely away from us
+KERNEL_OFFSET equ 0x1000 ; The same one we used when linking the kernel
+
+    mov [BOOT_DRIVE], dl ; Remember that the BIOS sets us the boot drive in 'dl' on boot
+    mov bp, 0x9000 ; set the stack safely away from us
     mov sp, bp
 
-    mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
-    mov dh, 2 ; read 2 sectors
-    call disk_load
-
-    mov dx, [0x9000] ; retrieve first loaded word, 0xdada
-    call print_hex
-
+    mov bx, MSG_REAL_MODE
+    call print
     call print_nl
 
-    mov dx, [0x9000 + 512] ; first word from loaded sector, 0xface
-    call print_hex
+    call load_kernel ; read the kernel from disk
+    call switch_to_pm ; disable interrupts, load GTD, etc. Finally jumps to 'BEGIN_PM'
+    jmp $ ; never executed
 
-    ; Done! We can hang now
-    jmp $
+%include "boot_sect/boot_sect_print.asm"
+%include "boot_sect/boot_sect_print_hex.asm"
+%include "boot_sect/boot_sect_disk.asm"
+%include "32bit/32bit-gdt.asm"
+%include "32bit/32bit-print.asm"
+%include "32bit/32bit-switch.asm"
 
-; Dont forget to include subroutines below the hang
-%include "boot_sect_print.asm"
-%include "boot_sect_print_hex.asm"
-%include "boot_sect_disk.asm"
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print
+    call print_nl
+
+    mov bx, KERNEL_OFFSET ; read from disk and store in 0x1000
+    mov dh, 2
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
+
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    call KERNEL_OFFSET ; Give control to the kernel
+    jmp $ ; Stay here when the kernel returns control to us (if ever)
+
+
+BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Started in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory", 0
 
 ; padding and magic number
 times 510-($-$$) db 0
 dw 0xaa55
-
-; boot sector = sector 1 of cyl 0 of head 0 of hdd 0
-; from now on = sector 2 ... 
-times 256 dw 0xdada ; sector 2 = 512 bytes
-times 256 dw 0xface ; sector 3 = 512 bytes
