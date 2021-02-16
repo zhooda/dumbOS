28c571256f81e66a59fcc8895964afc31e0edc9f
Zeeshan Hooda <zhooda@protonmail.com>
2021-02-16 09:40:00 -0700

    reset again

---
+++ .old/2021/src/lib.rs
@@ -0,0 +1,9 @@
+#![feature(lang_items)]
+#![no_std]
+#[allow(non_snake_case)]
+
+#[no_mangle]
+pub extern fn rust_main() {}
+
+#[lang = "eh_personality"] #[no_mangle] pub extern fn eh_personality() {}
+#[lang = "panic_fmt"] #[no_mangle] pub extern fn panic_fmt() -> ! {loop{}}
